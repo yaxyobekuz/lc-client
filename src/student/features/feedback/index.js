4eab3ae97221cbdd1c7452b9122a14335570da38
yaxyobekuz <pubgn9642@gmail.com>
2026-05-10 22:48:32 +0500

    feat: add MyFeedbackPage component for student feedback functionality

---
+++ src/student/features/feedback/index.js
@@ -0,0 +1,1 @@
+export { default as MyFeedbackPage } from "./pages/MyFeedbackPage";
---
+++ src/student/features/feedback/pages/MyFeedbackPage.jsx
@@ -0,0 +1,6 @@
+import { MyFeedbackPage as TeacherMyFeedbackPage } from "@/teacher/features/feedback";
+
+// Teacher panel'dagi sahifa o'qituvchi va talaba uchun bir xil ishlaydi
+const MyFeedbackPage = () => <TeacherMyFeedbackPage />;
+
+export default MyFeedbackPage;
