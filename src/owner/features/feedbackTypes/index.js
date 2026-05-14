e5d7006f9bce1b0c6c9d5a3a67798a4ab0252476
yaxyobekuz <pubgn9642@gmail.com>
2026-05-14 11:16:15 +0500

    feat: remove feedback types feature and related code

--- src/owner/features/feedbackTypes/index.js
+++
@@ -1,8 +0,0 @@
-export { default as FeedbackTypesListPage } from "./pages/FeedbackTypesListPage";
-export { feedbackTypesAPI } from "./api/feedbackTypes.api";
-export { default as useFeedbackTypesQuery } from "./hooks/useFeedbackTypesQuery";
-export {
-  useFeedbackTypeCreateMutation,
-  useFeedbackTypeUpdateMutation,
-  useFeedbackTypeRemoveMutation,
-} from "./hooks/useFeedbackTypeMutations";
